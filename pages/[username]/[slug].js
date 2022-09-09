49d23f4af0a8dc1f49ac570ac46dd5341b78196b
Pitchnogle <pitchnogle@gmail.com>
2022-09-09 15:25:56 -0600

    add incremental static regeneration

--- pages/[username]/[slug].js
+++ pages/[username]/[slug].js
@@ -1,7 +1,48 @@
-export default function PostPage({}) {
-  return (
-    <main>
-      <h2>A Post</h2>
-    </main>
-  );
+import styles from "../../styles/Post.module.css";
+import PostContent from "../../components/PostContent";
+import { firestore, getUserWithUsername, postToJSON } from "../../lib/firebase";
+import { useDocumentData } from "react-firebase-hooks/firestore";
+
+export async function getStaticProps({ params }) {
+  const { username, slug } = params;
+  const userDoc = await getUserWithUsername(username);
+
+  let post;
+  let path;
+
+  if (userDoc) {
+    const postsRef = userDoc.ref.collection("posts").doc(slug);
+    post = postToJSON(await postsRef.get());
+    path = postsRef.path;
+  }
+
+  return {
+    props: { post, path },
+    revalidate: 5000,
+  };
+}
+
+export async function getStaticPaths() {
+  // Improve my using Admin SDK to select empty docs
+  const snapshot = await firestore.collectionGroup("posts").get();
+
+  const paths = snapshot.docs.map((doc) => {
+    const { slug, username } = doc.data();
+    return {
+      params: { username, slug },
+    };
+  });
+
+  return {
+    // Must be in this format:
+    // paths: [
+    //   { params: { username, slug }}
+    // ],
+    paths,
+    fallback: "blocking",
+  };
+}
+
+export default function Post(props) {
+  return <main className={styles.container}></main>;
 }
